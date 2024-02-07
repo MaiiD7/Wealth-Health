f2f216cd4993363e4bafbd34d2b7cfdbfa2805f1
Mairo Dronsart <mairo.dronsart@csgroup.eu>
2024-02-07 18:11:53 +0100

    Vite JS Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      Hello World
+    </div>
+  </React.StrictMode>,
+)
